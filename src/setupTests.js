445b9a85cbb115c2a22e4a0c840c4e08975ecbca
Kristoffergidd <72502045+Kristoffergidd@users.noreply.github.com>
2024-05-31 18:42:07 +0200

    as

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,6 +2,7 @@ import { afterAll, afterEach, beforeAll } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 import { server } from "./mocks/setup";
+import "cross-fetch/polyfill";
 
 afterEach(() => {
   cleanup();
